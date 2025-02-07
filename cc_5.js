3777992e3ff61187f368eb754864face110a7598
Vaclav Ruzicka <vaclavruzicka@usf.edu>
2025-02-06 23:37:41 -0500

    Started Task 2, will come back later.

--- cc_5.js
+++ cc_5.js
@@ -8,4 +8,17 @@ let customer = {
 //Using a for-in loop to log each property using a template literal.
 for (const key in customer) {
     console.log(`${key}: ${customer[key]}`)
-    }
+}
+
+//Task 2 - Object Methods
+
+//Declaring an object named order with the properties orderID, total amount, and status.
+let order = {
+    orderID: 12345, totalAmount: 150, status: "Prosessing"
+}
+
+//Adding a method named displayOrder to order the details by retrieving the details using "this".
+order.displayOrder = function() {
+    return this.orderID, this.totalAmount, this.status
+}
+console.log(order.displayOrder())
